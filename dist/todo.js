09da1b78d9338524d46998458fb07d0dcf8c8098
Raiyan Ahmed <ray38250968@gmail.com>
2025-01-02 11:37:45 +0530

    typescript failed configurations

---
+++ dist/todo.js
@@ -0,0 +1,44 @@
+"use strict";
+export default class Todo {
+    getTodoFromStorage() {
+        let todo = JSON.parse(localStorage.getItem("todo") || "null");
+        if (!todo) {
+            todo = {
+                tasks: [
+                    {
+                        name: "Your Task goes here",
+                        completed: false,
+                    },
+                    {
+                        name: "Your completed Task goes here",
+                        completed: true,
+                    },
+                    {
+                        name: "Your Task goes here",
+                        completed: false,
+                    },
+                    {
+                        name: "Your completed Task goes here",
+                        completed: true,
+                    },
+                ],
+                recycleBin: [
+                    {
+                        name: "Your deleted Task goes here",
+                        completed: true,
+                    },
+                    {
+                        name: "Your deleted Task goes here",
+                        completed: true,
+                    },
+                ],
+            };
+            localStorage.setItem("todo", JSON.stringify(todo));
+        }
+        console.log(todo);
+        return todo;
+    }
+    constructor() {
+        this.todo = this.getTodoFromStorage();
+    }
+}
